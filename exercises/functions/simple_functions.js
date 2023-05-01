4a167a5474ff9d27a7254e3943fe22e7b410de34
Jacob DeFalco <jake.defalco97@gmail.com>
2023-05-01 11:26:12 -0400

    init commit

---
+++ exercises/functions/simple_functions.js
@@ -0,0 +1,20 @@
+"use strict";
+function favoritethings() {
+  console.log(`My name is Jake and a few of my favorite things are: `);
+  console.log(`Movie - The Greatest Showman`);
+  console.log(`Artist - $uicideboy$`);
+}
+
+favoritethings();
+
+function whyImHere() {
+  console.log(`The reason I am in the LearnToCode program is to advance my career in a field I feel passionate about!`);
+}
+
+whyImHere();
+
+function favoritePlace() {
+  console.log(`One of my favorite places to visit is my grandma's house becuase she makes sure I'm fed everytime.`);
+}
+
+favoritePlace();
